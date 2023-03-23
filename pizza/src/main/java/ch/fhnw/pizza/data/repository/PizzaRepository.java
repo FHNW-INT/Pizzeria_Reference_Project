75e5ef6c3778f92537cb9dca19e5bf5908b868c9
charutapande <32811202+charutapande@users.noreply.github.com>
2023-03-23 11:08:22 +0100

    config changes

--- pizza/src/main/java/ch/fhnw/pizza/data/repository/PizzaRepository.java
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/PizzaRepository.java
@@ -3,9 +3,11 @@ package ch.fhnw.pizza.data.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ch.fhnw.pizza.data.domain.Pizza;
 
+@Repository
 //JpaRepository should be typed to the domain class and an ID type
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
     Pizza findByPizzaName(String pizzaName);
