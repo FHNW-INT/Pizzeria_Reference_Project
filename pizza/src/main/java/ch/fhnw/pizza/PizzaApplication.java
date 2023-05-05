3366151bf1e117c36b3f4a06043073883306fa1c
charutapande <32811202+charutapande@users.noreply.github.com>
2023-05-05 08:30:57 +0000

    Initialize placeholder data for testing

--- pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
+++ pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
@@ -1,13 +1,21 @@
 package ch.fhnw.pizza;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.fhnw.pizza.business.service.MenuService;
+import ch.fhnw.pizza.data.domain.Pizza;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class PizzaApplication {
+
+	@Autowired
+	private MenuService menuService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaApplication.class, args);
@@ -18,5 +26,22 @@ public class PizzaApplication {
         
         return "Pizza Takeaway Service, welcome!";
     } 
+
+	// Use this method to initialize placeholder data without using Postman
+	// If you are persisting data in a file (see application.properties), initializing data that already exists will cause an error during starting the application
+	// To resolve the error, delete the file and restart the application
+	@PostConstruct
+	private void initPlaceholderData() throws Exception {
+		Pizza pizza = new Pizza();
+		pizza.setPizzaName("Margherita");
+		pizza.setPizzaToppings("Tomato sauce, mozzarella, basil");
+		menuService.addPizza(pizza);
+
+		pizza = new Pizza();
+		pizza.setPizzaName("Funghi");
+		pizza.setPizzaToppings("Tomato sauce, mozzarella, mushrooms");
+		menuService.addPizza(pizza);
+		
+	}
 
 }
