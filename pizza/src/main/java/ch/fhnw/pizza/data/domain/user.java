96162fe9e6067b4a04da2e5992d11c5db32a230a
Omar Rahiel <130754645+omar7l@users.noreply.github.com>
2024-05-15 14:46:44 +0200

    Create user.java

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/domain/user.java
@@ -0,0 +1,77 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "user_firstname", nullable = false)
+    private String userFirstname;
+
+    @Column(name = "user_lastname", nullable = false)
+    private String userLastname;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "user_username", nullable = false)
+    private String userUsername;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserFirstname() {
+        return userFirstname;
+    }
+
+    public void setUserFirstname(String userFirstname) {
+        this.userFirstname = userFirstname;
+    }
+
+    public String getUserLastname() {
+        return userLastname;
+    }
+
+    public void setUserLastname(String userLastname) {
+        this.userLastname = userLastname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
