02ebea1468cefcd7bbeb356faea7679c1336ca5f
Omar Rahiel <130754645+omar7l@users.noreply.github.com>
2024-05-15 14:42:33 +0200

    Create degree.java

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/domain/degree.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "degree")
+public class Degree {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "degree_id", nullable = false)
+    private Long degreeId;
+
+    @Column(name = "degree_name", nullable = false)
+    private String degreeName;
+
+    // Getters and Setters
+    public Long getDegreeId() {
+        return degreeId;
+    }
+
+    public void setDegreeId(Long degreeId) {
+        this.degreeId = degreeId;
+    }
+
+    public String getDegreeName() {
+        return degreeName;
+    }
+
+    public void setDegreeName(String degreeName) {
+        this.degreeName = degreeName;
+    }
+}
