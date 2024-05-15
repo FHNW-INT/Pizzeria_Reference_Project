25256bdc9c996865d28e6b70cb38c0be257dbd88
Omar Rahiel <130754645+omar7l@users.noreply.github.com>
2024-05-15 14:45:38 +0200

    Create location.java

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/domain/location.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "location_id", nullable = false)
+    private Long locationId;
+
+    @Column(name = "location_name", nullable = false)
+    private String locationName;
+
+    // Getters and Setters
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
