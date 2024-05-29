babc876ef8d1c27656de8ed4c39c137e7de8bd52
Omar Rahiel <130754645+omar7l@users.noreply.github.com>
2024-05-29 13:32:23 +0200

    Update TeacherService.java

--- pizza/src/main/java/ch/noteshub/fhnw/business/service/TeacherService.java
+++ pizza/src/main/java/ch/noteshub/fhnw/business/service/TeacherService.java
@@ -1,1 +1,45 @@
+package ch.noteshub.fhnw.business.service;
+
+import ch.noteshub.fhnw.data.domain.Teacher;
+import ch.noteshub.fhnw.data.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeacherService {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    public Teacher findTeacherById(Long id) {
+        Optional<Teacher> teacherOptional = teacherRepository.findById(id);
+        if (teacherOptional.isPresent()) {
+            return teacherOptional.get();
+        } else {
+            throw new RuntimeException("Teacher with id " + id + " not found");
+        }
+    }
+
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+    public Teacher addTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Teacher updateTeacher(Long id, Teacher teacherDetails) {
+        Teacher teacher = findTeacherById(id);
+        teacher.setTeacherFirstname(teacherDetails.getTeacherFirstname());
+        teacher.setTeacherLastname(teacherDetails.getTeacherLastname());
+        return teacherRepository.save(teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        teacherRepository.deleteById(id);
+    }
+}
 
