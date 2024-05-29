6efcf35ee78e2e9d35280d8389a07a7399cc049e
Omar Rahiel <130754645+omar7l@users.noreply.github.com>
2024-05-29 13:27:26 +0200

    Create UserController.java

---
+++ pizza/src/main/java/ch/noteshub/fhnw/controller/UserController.java
@@ -0,0 +1,66 @@
+package ch.noteshub.fhnw.controller;
+
+import ch.noteshub.fhnw.data.domain.User;
+import ch.noteshub.fhnw.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User userToUpdate = user.get();
+            userToUpdate.setUserFirstname(userDetails.getUserFirstname());
+            userToUpdate.setUserLastname(userDetails.getUserLastname());
+            userToUpdate.setUserEmail(userDetails.getUserEmail());
+            userToUpdate.setUserUsername(userDetails.getUserUsername());
+            userToUpdate.setUserPassword(userDetails.getUserPassword());
+            return ResponseEntity.ok(userRepository.save(userToUpdate));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
